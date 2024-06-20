d307d7eb217b6f9e20160e2cc90c1cb646ad6fac
mabdulam <mohammed.abdul-ameer@outlook.com>
2024-06-20 01:19:18 -0400

    TOC updated with link to branch a3

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -32,7 +32,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/mabdulam/kanbas-react-web-app" className="nav-link">
+        <a id="wd-k" href="https://github.com/mabdulam/kanbas-react-web-app/tree/a3" className="nav-link">
           My GitHub
         </a>
       </li>
